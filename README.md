96b55890f1ad8c68a563ea26e5e3ca5059aadb07
miyay <yuuma_117@hotmail.com>
2015-06-03 01:41:58 +0900

    1st commit(仮)

---
+++ README.md
@@ -0,0 +1,3 @@
+# Dandelion Girl
+
+This site is a sample page that uses gumby and Ruby on Rails.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DandelionGirl::Application.config.secret_token = 'd5244059f4940c3c184728dd3ee9e8cc9f9d047de16b5221c9ce45f7f6acbda385ba52f1bc2a92e46f09cfcb49125eb159f897395ddb2d69749db56abf8e7c13'
